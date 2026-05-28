8f9243add4014d8f75d17c95660c2f9c0d87b331
Zhang Jingqiang <zh_jq@outlook.com>
2026-05-28 23:19:20 +0800

    allow vendored build of mimalloc by using cmake

--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 
 Rust FFI binding to [mimalloc](https://github.com/microsoft/mimalloc).
 
-It will find a pre-installed jemalloc via pkg-config or vcpkg instead of build a vendored version.
+It will find a pre-installed jemalloc via pkg-config or vcpkg if the "vendored" feature is not selected.
+And the "secure" feature will enable a secure vendored build of mimalloc.
--- build.rs
+++ build.rs
@@ -3,10 +3,15 @@ use std::path::PathBuf;
 
 use bindgen::{MacroTypeVariation, RustEdition};
 
+#[cfg(not(feature = "vendored"))]
 fn main() {
+    let include_dirs = probe_installed().unwrap_or_default();
+    do_bindgen(include_dirs);
+}
+
+fn do_bindgen(include_dirs: Vec<PathBuf>) {
     let out_dir = PathBuf::from(env::var_os("OUT_DIR").unwrap());
     let bindgen_file = out_dir.join("bindgen.rs");
-    let include_dirs = probe_installed().unwrap_or_default();
 
     let mut builder = bindgen::Builder::default();
     if let Some(dir) = include_dirs.first() {
@@ -28,6 +33,7 @@ fn main() {
         .expect("Couldn't write bindings!");
 }
 
+#[cfg(not(feature = "vendored"))]
 fn probe_installed() -> Option<Vec<PathBuf>> {
     if let Ok(lib) = pkg_config::Config::new()
         .print_system_libs(false)
@@ -45,4 +51,44 @@ fn probe_installed() -> Option<Vec<PathBuf>> {
 
     println!("cargo:rustc-link-lib=mimalloc");
     None
+}
+
+#[cfg(feature = "vendored")]
+fn main() {
+    use std::path::Path;
+
+    let crate_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
+    let src_dir = Path::new(&crate_dir);
+    let mimalloc_src_dir = src_dir.join("mimalloc");
+    let include_dir = mimalloc_src_dir.join("include");
+
+    println!("cargo:rerun-if-changed={}", mimalloc_src_dir.display());
+    let mut cmake_config = cmake::Config::new(mimalloc_src_dir);
+    cmake_config
+        .define("MI_BUILD_SHARED", "OFF")
+        .define("MI_BUILD_STATIC", "ON")
+        .define("MI_BUILD_TESTS", "OFF")
+        .define("MI_OPT_ARCH", "ON")
+        .define("MI_OPT_SIMD", "ON");
+    #[cfg(target_env = "musl")]
+    cmake_config.define("MI_LIBC_MUSL", "ON");
+    #[cfg(feature = "secure")]
+    cmake_config.define("MI_SECURE", "ON");
+
+    let mimalloc_build_dir = cmake_config.build();
+    let lib_search_dir = Path::new(&mimalloc_build_dir).join("build");
+    // set link options
+    println!(
+        "cargo:rustc-link-search=native={}",
+        lib_search_dir.display()
+    );
+    let mut libname = String::from("mimalloc");
+    #[cfg(feature = "secure")]
+    libname.push_str("-secure");
+    if cmake_config.get_profile() == "Debug" {
+        libname.push_str("-debug");
+    }
+    println!("cargo:rustc-link-lib=static={libname}");
+
+    do_bindgen(vec![include_dir]);
 }
